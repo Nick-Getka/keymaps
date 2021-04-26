e957262d2d7eab3f1b736f85a88496676f8f7b65
Nick Getka <nick@reviewpush.com>
2021-04-25 19:16:56 -0500

    Added functions to nav to apps and sites. Removed unnecessary tilde.  Fixed custom shift macros

--- javascript/keymap.c
+++ javascript/keymap.c
@@ -35,6 +35,9 @@ enum planck_keycodes {
   VSCODE,
   TERM,
   CHROME,
+  REDDIT,
+  GOOGLE,
+  YOUTUBE,
 };
 
 #define LOWER MO(_LOWER)
@@ -52,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      | Ctrl | GUI  |  Alt |Lower |    Space    |Raise | Left |  Up  | Down |Right |
  * `-----------------------------------------------------------------------------------'
- */Term
+ */
 [_QWERTY] = LAYOUT_planck_grid(
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
@@ -67,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |PgDown| Left | Down |Right | Copy |Paste |  *   | =/+  | (/)  | {/}  |  |   |   `  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |SLeft |C.Row |SRight|  ~   |  @   |  %   |  #   | </>  | ./,  |  &   |      |
+ * |      |SLeft |C.Row |SRight|      |  @   |  %   |  #   | </>  | ./,  |  &   |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      |ALeft | Spot |Finder|ARight|
  * `-------------------------------------------------------- ---------------------------'
@@ -75,25 +78,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT_planck_grid(
     KC_PGUP,   KC_HOME,       KC_UP,   KC_END ,        G(KC_X), G(KC_F),  S(KC_6), KC_MINS, SQUAR,     S(KC_4),     S(KC_1),        KC_DEL,
     KC_PGDOWN, KC_LEFT,       KC_DOWN, KC_RGHT,        G(KC_C), G(KC_V),  S(KC_8), KC_EQL,  PAREN,     BRACK,       S(KC_BSLS),     KC_GRAVE,
-    _______,   SGUI(KC_LEFT), G(KC_D), SGUI(KC_RIGHT), S(KC_GRV), S(KC_1),  S(KC_5), S(KC_3),  ANGLE,     DOT,         S(KC_7),        _______,
+    _______,   SGUI(KC_LEFT), G(KC_D), SGUI(KC_RIGHT), _______, S(KC_1),  S(KC_5), S(KC_3),  ANGLE,     DOT,         S(KC_7),        _______,
     _______,   _______,       _______, _______,        _______, _______,  _______, _______, G(KC_TAB), G(KC_SPACE), G(A(KC_SPACE)), SGUI(KC_TAB)
 ),
 
 /* Raise
  * ,-----------------------------------------------------------------------------------.
- * | UTab |TLeft |NewTab|TRight|VSCode|      | All  |Save  |   1  |   2  |   3  | Del  |
+ * | UTab |TLeft |NewTab|TRight|VSCode|Reddit| All  |Save  |   1  |   2  |   3  | Del  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |CloseT|Tools |Reload| Term |      | Redo | Undo |   4  |   5  |   6  |  0   |
+ * |      |CloseT|Tools |Reload| Term |Google| Redo | Undo |   4  |   5  |   6  |  0   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |CloseW|NewWin|Chrome|      |      |      |      |   7  |   8  |   9  |  .   |
+ * |      |CloseW|NewWin|Chrome|      |UTube |      |      |   7  |   8  |   9  |  .   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      |DLeft | DUp  |DDown |DRight|
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_planck_grid(
-    S(KC_TAB), C(S(KC_TAB)), G(KC_T),    C(KC_TAB), VSCODE,  _______, G(KC_A),    G(KC_S), KC_1,          KC_2,        KC_3,           KC_DEL,
-    _______,   G(KC_W),      G(A(KC_I)), G(KC_R),   TERM,    _______, SGUI(KC_Z), G(KC_Z), KC_4,          KC_5,        KC_6,           KC_0,
-    _______,   G(S(KC_W)),   G(KC_N),    CHROME,    _______, _______, _______,    _______, KC_7,          KC_8,        KC_9,           KC_DOT,
+    S(KC_TAB), C(S(KC_TAB)), G(KC_T),    C(KC_TAB), VSCODE,  REDDIT,  G(KC_A),    G(KC_S), KC_1,          KC_2,        KC_3,           KC_DEL,
+    _______,   G(KC_W),      G(A(KC_I)), G(KC_R),   TERM,    GOOGLE,  SGUI(KC_Z), G(KC_Z), KC_4,          KC_5,        KC_6,           KC_0,
+    _______,   G(S(KC_W)),   G(KC_N),    CHROME,    _______, YOUTUBE, _______,    _______, KC_7,          KC_8,        KC_9,           KC_DOT,
     _______,   _______,      _______,    _______,   _______, _______, _______,    _______, LCTL(KC_LEFT), LCTL(KC_UP), LCTL(KC_DOWN),  LCTL(KC_RIGHT)
 ),
 
@@ -126,6 +129,33 @@ layer_state_t layer_state_set_user(layer_state_t state) {
   return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
 }
 
+void open_app(char app[]){
+  register_code(KC_LGUI);
+  register_code(KC_SPACE);
+  clear_keyboard();
+  SEND_STRING(app);
+  register_code(KC_ENTER);
+  clear_keyboard();
+}
+
+void open_site(char site[]){
+  register_code(KC_LGUI);
+  register_code(KC_SPACE);
+  clear_keyboard();
+  SEND_STRING("Google Chrome");
+  register_code(KC_ENTER);
+  clear_keyboard();
+  register_code(KC_LGUI);
+  register_code(KC_T);
+  clear_keyboard();
+  register_code(KC_LGUI);
+  register_code(KC_L);
+  clear_keyboard();
+  SEND_STRING(site);
+  register_code(KC_ENTER);
+  clear_keyboard();
+}
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case QWERTY:
@@ -134,10 +164,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         set_single_persistent_default_layer(_QWERTY);
       }
       return false;
-    break;
-  case PAREN:
+      break;
+    case PAREN:
       if (record->event.pressed) {
         if (keyboard_report->mods & MOD_BIT(KC_LSFT) || keyboard_report->mods & MOD_BIT(KC_RSFT)){
+          clear_mods();
           register_code(KC_LSFT);
           register_code(KC_0);
         } else {
@@ -151,11 +182,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case BRACK:
       if (record->event.pressed) {
         if (keyboard_report->mods & MOD_BIT(KC_LSFT) || keyboard_report->mods & MOD_BIT(KC_RSFT)){
-          register_code(KC_LSFT);
-          register_code(KC_LBRACKET);
-        } else {
+          clear_mods();
           register_code(KC_LSFT);
           register_code(KC_RBRACKET);
+        } else {
+          register_code(KC_LSFT);
+          register_code(KC_LBRACKET);
         }
         clear_keyboard();
       }
@@ -164,11 +196,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case ANGLE:
       if (record->event.pressed) {
         if (keyboard_report->mods & MOD_BIT(KC_LSFT) || keyboard_report->mods & MOD_BIT(KC_RSFT)){
-          register_code(KC_LSFT);
-          register_code(KC_COMMA);
-        } else {
+          clear_mods();
           register_code(KC_LSFT);
           register_code(KC_DOT);
+        } else {
+          register_code(KC_LSFT);
+          register_code(KC_COMMA);
         }
         clear_keyboard();
       }
@@ -177,9 +210,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case SQUAR:
       if (record->event.pressed) {
         if (keyboard_report->mods & MOD_BIT(KC_LSFT) || keyboard_report->mods & MOD_BIT(KC_RSFT)){
-          register_code(KC_LBRACKET);
-        } else {
+          clear_mods();
           register_code(KC_RBRACKET);
+        } else {
+          register_code(KC_LBRACKET);
         }
         clear_keyboard();
       }
@@ -188,6 +222,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case DOT:
       if (record->event.pressed) {
         if (keyboard_report->mods & MOD_BIT(KC_LSFT) || keyboard_report->mods & MOD_BIT(KC_RSFT)){
+          clear_mods();
           register_code(KC_DOT);
         } else {
           register_code(KC_COMMA);
@@ -198,34 +233,37 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       break;
     case VSCODE:
       if (record->event.pressed) {
-        register_code(KC_LGUI);
-        register_code(KC_SPACE);
-        clear_keyboard();
-        SEND_STRING("Visual Studio Code");
-        register_code(KC_ENTER);
-        clear_keyboard();
+        open_app("Visual Studio Code");
       }
       return false;
       break;
     case TERM:
       if (record->event.pressed) {
-        register_code(KC_LGUI);
-        register_code(KC_SPACE);
-        clear_keyboard();
-        SEND_STRING("Terminal");
-        register_code(KC_ENTER);
-        clear_keyboard();
+        open_app("Terminal");
       }
       return false;
       break;
     case CHROME:
       if (record->event.pressed) {
-        register_code(KC_LGUI);
-        register_code(KC_SPACE);
-        clear_keyboard();
-        SEND_STRING("Google Chrome");
-        register_code(KC_ENTER);
-        clear_keyboard();
+        open_app("Google Chrome");
+      }
+      return false;
+      break;
+    case REDDIT:
+      if (record->event.pressed) {
+        open_site("https://www.reddit.com/");
+      }
+      return false;
+      break;
+    case GOOGLE:
+      if (record->event.pressed) {
+        open_site("https://www.google.com/");
+      }
+      return false;
+      break;
+    case YOUTUBE:
+      if (record->event.pressed) {
+        open_site("https://www.youtube.com/");
       }
       return false;
       break;
